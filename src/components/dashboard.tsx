a207c6e96013f62f7e6ec3af4d922bef07d56c10
Raghav <raghav.tandon13@gmail.com>
2024-07-15 16:08:38 +0530

    dashboard with all lender and a ON/OFF active status

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -38,24 +38,37 @@ const Dashboard = ({ dates }: any) => {
         return (
             <div className="flex w-full flex-col items-center justify-center py-2 sm:py-8">
                 <BarLoader loading={loading} aria-label="Loading Spinner" />
-		<p className='pt-2'>aggregating... please wait</p>
+                <p className="pt-2">aggregating... please wait</p>
             </div>
         );
     }
 
+    const getStatus = (key: any) => {
+        if (key in data.data.active) {
+            return data.data.active[key] === true ? (
+                <span className="text-green-500">ON</span>
+            ) : (
+                <span className="text-gray-300">OFF</span>
+            );
+        }
+        return;
+    };
     return (
         <div className="gap-2">
             <Table>
                 <TableBody>
-                    {Object.entries(data.data).map(
-                        ([key, value]: any) =>
-                            value && (
-                                <TableRow key={key}>
-                                    <TableCell className="font-medium">{key}</TableCell>
-                                    <TableCell className="text-right">{value.toString()}</TableCell>
-                                </TableRow>
-                            ),
-                    )}
+                    {Object.entries(data.data)
+                        .filter(([key]) => key !== 'active')
+                        .map(
+                            ([key, value]: any) =>
+                                value && (
+                                    <TableRow key={key}>
+                                        <TableCell className="w-[40px]">{getStatus(key)}</TableCell>
+                                        <TableCell className="font-medium">{key} Leads</TableCell>
+                                        <TableCell className="text-right">{value.toString()}</TableCell>
+                                    </TableRow>
+                                ),
+                        )}
                 </TableBody>
             </Table>
         </div>
