eeaed39eead627c1cd06e357dcb85bc26c67c6ff
Raghav <raghav.tandon13@gmail.com>
2024-07-26 14:23:27 +0530

    countboard is live (major)

--- src/app/full/page.tsx
+++ src/app/full/page.tsx
@@ -2,6 +2,9 @@ import Image from 'next/image';
 import Search from '@/components/search';
 import Date from '@/components/date';
 import Countboard from '@/components/countboard';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 interface SearchParams {
     start?: string;
@@ -21,14 +24,29 @@ export default function Full({ searchParams }: FullProps) {
     return (
         <div className="flex min-h-screen flex-col items-stretch p-10 px-5 md:p-20 md:px-40">
             <div className="flex h-20 w-full items-end justify-between">
-                <a href="/" className="text-2xl font-bold">
-                    DATABASE
-                </a>
+                <Link href="/" className="text-2xl font-bold">
+                    <Button className="ml-[-1rem] text-2xl font-bold" variant={'link'}>
+                        DATABASE
+                    </Button>
+                </Link>
                 <Image className="rounded" src="/cred.svg" alt="Credmantra Logo" width={150} height={36} priority />
             </div>
             <Search phone="" />
             <h1 className="flex flex-col items-start py-5 text-2xl font-bold sm:flex-row sm:items-center">
-                DASHBOARD
+                <TooltipProvider>
+                    <Tooltip>
+                        <TooltipTrigger>
+                            <Link href="/">
+                                <Button className="ml-[-1rem] text-2xl font-bold" variant={'link'}>
+                                    FULL DASHBOARD
+                                </Button>
+                            </Link>
+                        </TooltipTrigger>
+                        <TooltipContent>
+                            <p>SHOW LESS DETAILS</p>
+                        </TooltipContent>
+                    </Tooltip>
+                </TooltipProvider>
                 {searchParams.start && searchParams.end && (
                     <div className="flex">
                         <span className="text-sm font-normal text-gray-500 sm:pl-2">from {searchParams.start}</span>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import Image from 'next/image';
 import Search from '@/components/search';
 import Date from '@/components/date';
 import Dashboard from '@/components/dashboard';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 export default async function Home(searchParams: any) {
     let dateprops: any = {};
@@ -15,12 +18,28 @@ export default async function Home(searchParams: any) {
     return (
         <main className="flex min-h-screen flex-col items-stretch p-10 px-5 md:p-20 md:px-40">
             <div className="flex h-20 w-full items-end justify-between">
-                <a href="/" className="text-2xl font-bold">DATABASE</a>
+                <Link href="/" className="text-2xl font-bold">
+                    <Button className="ml-[-1rem] text-2xl font-bold" variant={'link'}>
+                        DATABASE
+                    </Button>
+                </Link>
                 <Image className="rounded" src="/cred.svg" alt="Credmantra Logo" width={150} height={36} priority />
             </div>
             <Search phone="" />
-            <h1 className="flex flex-col items-start py-5 text-2xl font-bold sm:flex-row sm:items-center">
-                DASHBOARD
+            <h1 className="flex items-stretch py-5 text-2xl font-bold sm:flex-row sm:items-center">
+                <TooltipProvider>
+                    <Tooltip>
+                        <TooltipTrigger>
+                            <Button className="ml-[-1rem] text-2xl font-bold" variant={'link'}>
+                                <Link href="/full">DASHBOARD</Link>
+                            </Button>
+                        </TooltipTrigger>
+                        <TooltipContent>
+                            <p>SHOW FULL DETAILS</p>
+                        </TooltipContent>
+                    </Tooltip>
+                </TooltipProvider>
+
                 {searchParams.searchParams.start && searchParams.searchParams.end ? (
                     <div className="flex">
                         <span className="text-sm font-normal text-gray-500 sm:pl-2">from {searchParams.searchParams.start}</span>
